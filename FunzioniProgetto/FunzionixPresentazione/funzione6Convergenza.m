12c298c60128de106a0f1a17e02485e02e697d40
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-06-25 10:59:00 +0200

    refactoring funzioni

--- FunzioniProgetto/FunzionixPresentazione/funzione6Convergenza.m
+++ FunzioniProgetto/FunzionixPresentazione/funzione6Convergenza.m
@@ -21,7 +21,7 @@ title('Decrescita del Residuo');
 C_reale = res(2:end) ./ res(1:end-1);
 
 figure()
-plot(C_reale, 'r.-', 'LineWidth', 1.5)
+plot(C_reale, 'r.-')
 xlabel('iter'); ylabel('Fattore C (res_{n}/res_{n-1})'); grid on
 title('Verifica del Fattore di Conversione Reale');
 
